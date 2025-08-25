0e16b535c32bdffd555d5a3557bbfa178a26c7f4
mooqa <oshkanov81@gmail.com>
2025-08-25 15:44:02 +0500

    new feature

--- ReaScripts/Rendering/misha - Create master fade-in and fade-out from regions.lua
+++ ReaScripts/Rendering/misha - Create master fade-in and fade-out from regions.lua
@@ -1,6 +1,6 @@
 -- @description Create master fade-in and Fade-out from regions
 -- @author Misha Oshkanov
--- @version 1.0
+-- @version 1.1
 -- @about
 --  Creates fade-in and fade-out on master track using regions named Fade-in and Fade-out.
 --  Script will remove all points on master track volume envelope before adding fades
@@ -8,8 +8,10 @@
 --  Cоздает фэйды на мастер треке, используя границы регионов с названиями Fade-in и Fade-out
 --  Скрипт удалит все точки с огибающей громкости на мастере перед созданием фэйдов
 
+USE_RENDER_FADES = true
 fade_in_name  = 'Fade-in'
 fade_out_name = 'Fade-out'
+-------------------------------------
 remove_all_points_on_start = true
 
 function print(...)
@@ -18,6 +20,39 @@ function print(...)
     if #values == 0 then values[1] = 'nil' end
     reaper.ShowConsoleMsg(table.concat(values, ' ') .. '\n')
 end
+
+function printt(t, indent)
+    indent = indent or 0
+    for k, v in pairs(t) do
+      if type(v) == "table" then
+        print(string.rep(" ", indent) .. k .. " = {")
+        printt(v, indent + 2)
+        print(string.rep(" ", indent) .. "}")
+      else
+        print(string.rep(" ", indent) .. k .. " = " .. tostring(v))
+      end
+    end
+end
+
+
+function save_selected_tracks()
+    tracks = {}
+    local count_sel_tracks = reaper.CountSelectedTracks(0)
+    if count_sel_tracks > 0 then 
+        for t=0,count_sel_tracks-1 do 
+            local track = reaper.GetSelectedTrack(0, t)
+            table.insert(tracks, track)
+        end 
+        return tracks
+    else return nil end
+end 
+
+
+function recall_selected_tracks(tracks)
+    for track=1, #tracks do 
+        reaper.SetTrackSelected(track, true)
+    end
+end 
 
 function remove_all_points_on_start()
     reaper.DeleteEnvelopePointRange(envelope, 0, reaper.GetProjectLength(0))
@@ -56,15 +91,26 @@ if envelope == nil then
     envelope = reaper.GetTrackEnvelopeByName(master, 'Volume')
 end
 
-if remove_all_points_on_start then remove_all_points_on_start() end
+if remove_all_points_on_start then 
+    remove_all_points_on_start()
+end
 
-_, _, num_regions = reaper.CountProjectMarkers(0)
-for i=0, num_regions do
+all, _, num_regions = reaper.CountProjectMarkers(0)
+for i=0, all do
     _, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
     if isrgn then 
-        if name == fade_in_name  then add_fade_in(pos,rgnend)  end
-        if name == fade_out_name then add_fade_out(pos,rgnend) end
-    end 
+        if USE_RENDER_FADES then 
+            reaper.GetSetProjectInfo(0, 'RENDER_NORMALIZE',  1542, 1)
+            if name == fade_in_name  then
+                reaper.GetSetProjectInfo(0, 'RENDER_FADEIN',  rgnend-pos, 1 )
+            end
+            if name == fade_out_name then
+                reaper.GetSetProjectInfo(0, 'RENDER_FADEOUT', rgnend-pos, 1 )
+            end
+        else 
+            if name == fade_in_name  then add_fade_in(pos,rgnend)  end
+            if name == fade_out_name then add_fade_out(pos,rgnend) end
+        end
+    end
 end 
-
-reaper.Undo_EndBlock( 'Create master fade-in and fade-out', -1 )
+reaper.Undo_EndBlock( 'Add master fades', -1 )
