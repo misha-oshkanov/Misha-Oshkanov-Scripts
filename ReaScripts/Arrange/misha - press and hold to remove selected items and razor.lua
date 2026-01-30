3cd2d49c688ce2a055b6d1f724be154ce5cf4179
mooqa <oshkanov81@gmail.com>
2026-01-30 13:31:11 +0500

    small update

--- ReaScripts/Arrange/misha - press and hold to remove selected items and razor.lua
+++ ReaScripts/Arrange/misha - press and hold to remove selected items and razor.lua
@@ -1,6 +1,6 @@
 -- @description press key to remove selected items and razor
 -- @author Misha Oshkanov
--- @version 1.0
+-- @version 1.1
 -- @about
 --    It deletes all selected items and razor when you press and hold script key button
 --    Ruler, peaks and item names change color if script is active
@@ -56,13 +56,13 @@ function Handle_errors(err)
 end
 
 
-function HexToInt( hex )
-  local r, g, b = HexToRGB( hex )
+function HexToInt(hex)
+  local r, g, b = HexToRGB(hex)
   local int =  reaper.ColorToNative( r, g, b )|16777216
   return int
 end
 
-function HexToRGB( hex )
+function HexToRGB(hex)
   local hex = hex:gsub("#","")
   local R = tonumber("0x"..hex:sub(1,2))
   local G = tonumber("0x"..hex:sub(3,4))
@@ -70,12 +70,12 @@ function HexToRGB( hex )
   return R, G, B
 end
 
-function SetButtonState( set )
+function SetButtonState(set)
   if not set then set = 0 end
   local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
-  local state = reaper.GetToggleCommandStateEx( sec, cmd )
+  local state = reaper.GetToggleCommandStateEx(sec, cmd)
   reaper.SetToggleCommandState( sec, cmd, set ) -- Set ON
-  reaper.RefreshToolbar2( sec, cmd )
+  reaper.RefreshToolbar2(sec, cmd)
 end
 
 function exit()
@@ -88,7 +88,7 @@ function exit()
     for k,item in ipairs(selected_items) do reaper.SetMediaItemSelected(item, 1) end
   end
   if RAZOR_ON_RIGHT_MB then 
-    reaper.SetMouseModifier( 'MM_CTX_ARRANGE_RMOUSE', 0, mouse_action)
+    reaper.SetMouseModifier('MM_CTX_ARRANGE_RMOUSE', 0, mouse_action)
   end
 
   reaper.UpdateTimeline()
@@ -100,6 +100,8 @@ function loop()
   reaper.Main_OnCommand(40006, 0)
   reaper.defer(function() xpcall(loop, Handle_errors) end)
 end
+
+reaper.Main_OnCommand(40312, 0)
 
 -- SetButtonState(1)
 selected_items = {}
@@ -115,11 +117,11 @@ end
 
 if RAZOR_ON_RIGHT_MB then 
   mouse_action = reaper.GetMouseModifier(  'MM_CTX_ARRANGE_RMOUSE', 0)
-  reaper.SetMouseModifier( 'MM_CTX_ARRANGE_RMOUSE', 0, action )
+  reaper.SetMouseModifier('MM_CTX_ARRANGE_RMOUSE', 0, action)
 end
 
-for i, v in ipairs( theme_elements_by_modes[1] ) do
-  reaper.SetThemeColor( v, HexToInt(color),  0)
+for i, v in ipairs(theme_elements_by_modes[1]) do
+  reaper.SetThemeColor(v, HexToInt(color),  0)
 end
 
 reaper.UpdateTimeline()
