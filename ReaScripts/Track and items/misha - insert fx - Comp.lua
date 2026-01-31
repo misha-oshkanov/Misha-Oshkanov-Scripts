0dc8ea6256d1cd445ab50dbc52ad17a959517a75
mooqa <oshkanov81@gmail.com>
2026-01-31 15:33:16 +0500

    monitor listen actions update and insert fx scripts

---
+++ ReaScripts/Track and items/misha - insert fx - Comp.lua
@@ -0,0 +1,53 @@
+-- @description insert or open UI of FX plugin on selected tracks
+-- @author Misha Oshkanov
+-- @version 1.0
+-- @about
+--  use to add and open desired fx on tracks
+--  1. If there is no fx on track, script will add new instance of fx
+--  2. If there is instance of fx on track, script will open the UI of last fx in chain
+--  3. if there is instance of fx and UI is open, script will add a new instance and open its UI
+--  You can use this script as template for new scripts
+
+
+FX = 'ReaComp' -- FX name
+
+---------------------------------------
+---------------------------------------
+---------------------------------------
+
+reaper.Undo_BeginBlock()
+
+function get_last_fx(track)
+  local count_fx = reaper.TrackFX_GetCount(track)
+  local FX = FX:lower()
+  local insert_FX = FX:gsub('-','%%-')
+  for fx=count_fx,0,-1 do 
+    local retval, fx_name = reaper.TrackFX_GetFXName(track, fx)
+    fx_name = fx_name:lower()
+    if fx_name:match(insert_FX) then return fx end
+  end 
+  return -1
+end
+
+track_id = 0
+track_count = reaper.CountSelectedTracks(0)
+while track_id < track_count do
+  track = reaper.GetSelectedTrack(0, track_id)
+  -- fx_on_track = reaper.TrackFX_AddByName(track, FX, false, 0)
+  fx_on_track = get_last_fx(track)
+  
+  if fx_on_track ~= -1 then 
+    is_open = reaper.TrackFX_GetOpen(track, fx_on_track)
+    if not is_open then 
+      reaper.TrackFX_SetOpen(track, fx_on_track, true)
+    else
+      reaper.TrackFX_SetOpen(track, fx_on_track, false)
+      reaper.TrackFX_AddByName(track, FX, false, -1)
+    end
+  else 
+    reaper.TrackFX_AddByName(track, FX, false, -1)
+  end
+  track_id =track_id+1
+end
+
+reaper.Undo_EndBlock("Insert FX: "..FX,-1)
--- ReaScripts/UI Utilities/misha - Monitor Volume Controller Shortcut - Listen Free.lua
+++ ReaScripts/UI Utilities/misha - Monitor Volume Controller Shortcut - Listen Free.lua
@@ -1,6 +1,6 @@
 -- @description Monitor Volume Controller Shortcut - Listen Free
 -- @author Misha Oshkanov
--- @version 0.2
+-- @version 0.3
 -- @about
 --  Action to solo certain frequency in monitor controller script
 
@@ -12,10 +12,13 @@ button_index = 6
 
 SLOPE = 2 -- 1 = 12db, 2 = 24db, 3 = 36db, 4 = 48db,5 = 60db, 6 = 72db 
 
+USE_METRICAB = true
+USE_METRIC_IN_MONITORINGFX = true
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
 master = reaper.GetMasterTrack()
 controller_fx = 'Monitor Volume Controller'
+METRIC_AB = 'ADPTR MetricAB'
 mon = (0x1000000)
 
 min_hz = 20
@@ -33,15 +36,59 @@ ext = tonumber(reaper.GetExtState('MISHA_MONITOR', 'LISTEN'))
 
 function print(msg) reaper.ShowConsoleMsg(tostring(msg) .. '\n') end
 
-function set_param_freq(master,param,value)
-    listen_index = reaper.TrackFX_AddByName(master, controller_fx, true, 100)
-    value = (math.log(value) - math.log(min_hz)) * (100 - 0) / (math.log(max_hz) - math.log(min_hz)) + 0
-    reaper.TrackFX_SetParam(master, listen_index+mon, param, value)
+function frequency_to_normalized(f)
+    local f_min = 10
+    local f_mid = 2637
+    local f_max = 22050
+    
+    local t = math.log(f / f_min) / math.log(f_mid / f_min)
+    
+    -- Проверяем СНАЧАЛА самые высокие частоты!
+    if f >= 4000 then
+        -- print('da')  -- Теперь это сработает для f >= 4000
+        t = t ^ 2.302
+    elseif f >= 801 then 
+        t = t ^ 2.374
+    elseif f >= 301 then
+        t = t ^ 2.59 
+    elseif f >= 61 then
+        t = t ^ 2.68  
+    elseif f >= 41 then
+        t = t ^ 2.51 
+    elseif f >= 10 then
+        t = t ^ 2.1 
+    end
+    
+    return 0.5 * t
 end
 
+function set_param_freq(master,param,value)
+  if USE_METRICAB then 
+    if param == 2 then param = 19
+    elseif param == 3 then param = 20 
+    end 
+    local index = reaper.TrackFX_AddByName(master, METRIC_AB, USE_METRIC_IN_MONITORINGFX, 0)
+    if index then 
+      if not USE_METRIC_IN_MONITORINGFX then mon = 0 else mon = (0x1000000) end
+      reaper.TrackFX_SetParam(master, index+mon, param, frequency_to_normalized(value))
+    end 
+  else 
+    listen_index = reaper.TrackFX_AddByName(master, controller_fx, true, 100)
+    value = (math.log(value) - math.log(min_hz)) * (100 - 0) / (math.log(max_hz) - math.log(min_hz)) + 0
+    reaper.TrackFX_SetParam(master, listen_index+(0x1000000), param, value)
+  end
+end
 function set_listen_state(master, state)
-    index = reaper.TrackFX_AddByName(master, controller_fx, true, 0)
-    reaper.TrackFX_SetParam(master, index+mon, 0, state)
+    if USE_METRICAB then 
+      local index = reaper.TrackFX_AddByName(master, METRIC_AB, USE_METRIC_IN_MONITORINGFX, 0)
+      if index then 
+        if not USE_METRIC_IN_MONITORINGFX then mon = 0 else mon = (0x1000000) end
+        reaper.TrackFX_SetParam(master, index+mon, 16, state)
+      end 
+    else
+        index = reaper.TrackFX_AddByName(master, controller_fx, true, 0)
+        reaper.TrackFX_SetParam(master, index+mon, 0, state)
+    end
 end 
 
 if ext == 0 or (ext > 0 and ext ~= button_index) then 
@@ -52,6 +99,7 @@ elseif ext == button_index then
     set_listen_state(master,0)
     reaper.SetExtState('MISHA_MONITOR', 'LISTEN', '0', true)
 end
+
 
 lowCut  = base_freq_ext / (2 ^ (base_width_ext / 2))
 highCut = base_freq_ext * (2 ^ (base_width_ext / 2))
